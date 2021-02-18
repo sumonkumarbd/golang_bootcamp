8fd89bbf4071d50ff6d2b4ae3844dfbc1cd5a216
[sumonkumarbd] <[sumonkmr.dev@gmail.com]>
2021-02-18 16:57:55 +0600

    I've add a function with arguments to pass same value

--- main.go
+++ main.go
@@ -9,11 +9,23 @@ func first_go() {
 
 	fmt.Println("hello world")
 }
+//end of first_go ()
+
+//start a function to test arguments
+func add_num(a , b int) int{
+   // return a + b
+   // return a - b
+   // return a * b
+    return a / b
+}
 
 // main function
 func main() {
 	// This is main function note::'all the programe will be able to exiqute in this main function.'
 
 	// to run day=01 programe
-	first_go()
+	//first_go()
+	
+	//run function add_num
+	fmt.Println(add_num(10,5))
 }
