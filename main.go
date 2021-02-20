60e4e1a0471f20c7d5cb5f8a9358772c8b99ea8b
[sumonkumarbd] <[sumonkmr.dev@gmail.com]>
2021-02-21 00:51:52 +0600

    add some pointer examples

--- main.go
+++ main.go
@@ -7,25 +7,31 @@ import "fmt"
 // Day=01 --lesson of run first golang code..
 func first_go() {
 
-	fmt.Println("hello world")
+  fmt.Println("hello world with golang")
 }
-//end of first_go ()
 
-//start a function to test arguments
-func add_num(a , b int) int{
-   // return a + b
-   // return a - b
-   // return a * b
-    return a / b
+
+//pointer examples
+func pointer(){
+var num *int
+var num1 int = 5
+    num = &num1
+
+fmt.Println(" first declear pointer variable is :",num ,"\n","num1 variable :", num1 ,"\n","num1 variable memory number is : " , num,"\n","After de-raffarance num value is : ",*num)
+
+
 }
+//end of pointer example
+
+
 
 // main function
 func main() {
-	// This is main function note::'all the programe will be able to exiqute in this main function.'
+  // This is main function note::'all the programe will be able to exiqute in this main function.'
 
-	// to run day=01 programe
-	//first_go()
-	
-	//run function add_num
-	fmt.Println(add_num(10,5))
+  // to run day=01 programe
+  // first_go()
+  
+  //run pointer function
+   pointer()
 }
